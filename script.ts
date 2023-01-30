6a7a8d97b1dc325fa9dcf911af4e9003c0ba0dd9
david <62385136+wang-626@users.noreply.github.com>
2023-01-30 19:45:32 +0800

    Add example data

---
+++ script.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: "yee@test.com",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
