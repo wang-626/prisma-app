6944f8e258c50455af739d0a62a07f7ea509d42e
david <62385136+wang-626@users.noreply.github.com>
2023-02-25 22:27:32 +0800

    Drop LoginToken column

---
+++ prisma/migrations/20230225142643_delete/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `LoginToken` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `LoginToken` DROP COLUMN `name`;
